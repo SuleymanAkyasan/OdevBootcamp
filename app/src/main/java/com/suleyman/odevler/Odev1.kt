0b52322c917137808bd3379365407aa5b5f09922
Suleyman Akyasan <vipsuleyman6134@gmail.com>
2025-04-27 18:32:27 +0300

    odev1 commit

---
+++ app/src/main/java/com/suleyman/odevler/Odev1.kt
@@ -0,0 +1,96 @@
+package com.suleyman.odevler
+
+fun main(){
+    val sehir = "Trabzon"
+    val ulke = "Türkiye"
+    var telefon : Long = 5346928561
+    val postCode : Int = 34674
+    var email = "suleymanakyasan6134@gmail.com"
+    var meslek = "Yazılım Mühendisi"
+    var stok_miktari = 0
+    var musteri_adi = "Fahriye"
+    var bakiye = 1500
+    val dogumGunu = "20.02.2001"
+    var medeniDurum = "Bekar"
+    var urun_yorum = "Cok beğendim çok güzeldi"
+    val odeme_tarihi = "20.02.2025"
+    var odeme = 15000
+    var siparis_adet = 37
+    var araba_model = "Mercedes"
+    val kitap_adi = "Suç ve Ceza"
+    val yayın_tarihi = "31.12.2023"
+    var indirim_miktari = 0.35
+    val oda_sayisi = 18
+    val enlem = 15
+    val boylam = 35
+    var urun_adi = "tutku"
+    var yemek_fiyati = 355
+    var marka = "Nike"
+    var muzik_adi = "Let It Happens"
+    var video_suresi = 15.36
+    var urun_puani = 4.8
+    var resim_adi = "Mona Lisa"
+    var dosya_format = ".png"
+    var renk = "Yeşil"
+    var renk_kod = "#4CAF50"
+    var telefon_model = "Iphone"
+    var ekran_boyutu = "5.6 inç"
+    var agırlık = "58 kg"
+    val ulusal_gun = "23 Nisan"
+    val tatil_gun = "Pazar"
+    var rezervasyon_tarih = "23.08.2025"
+    val sokak_ad = "Kürkçü Mümin"
+    var otobüsHattı = "Üsküdar-ümraniye"
+    var kalanDakika = 35
+    val takipKod = 18236798
+    val kuponSure = 18
+    val kuponKod = "NISAN2025"
+    var faturaAdres = "Üsküdar/İstanbul"
+
+    println(sehir)
+    println(ulke)
+    println(telefon)
+    println(postCode)
+    println(email)
+    println(meslek)
+    println(stok_miktari)
+    println(musteri_adi)
+    println(bakiye)
+    println(dogumGunu)
+    println(medeniDurum)
+    println(urun_yorum)
+    println(odeme_tarihi)
+    println(odeme)
+    println(siparis_adet)
+    println(araba_model)
+    println(kitap_adi)
+    println(yayın_tarihi)
+    println(indirim_miktari)
+    println(oda_sayisi)
+    println(enlem)
+    println(boylam)
+    println(urun_adi)
+    println(yemek_fiyati)
+    println(marka)
+    println(muzik_adi)
+    println(video_suresi)
+    println(urun_puani)
+    println(resim_adi)
+    println(dosya_format)
+    println(renk)
+    println(renk_kod)
+    println(telefon_model)
+    println(ekran_boyutu)
+    println(agırlık)
+    println(ulusal_gun)
+    println(tatil_gun)
+    println(rezervasyon_tarih)
+    println(sokak_ad)
+    println(otobüsHattı)
+    println(kalanDakika)
+    println(takipKod)
+    println(kuponSure)
+    println(kuponKod)
+    println(faturaAdres)
+
+}
